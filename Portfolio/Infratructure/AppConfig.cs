8d988bf9681e63ae0e9e65a88656a22639572afa
zhizhikkkk <zhanmamyt@bk.ru>
2025-08-04 16:59:02 +0600

    feat: index page and first setup

---
+++ Portfolio/Infratructure/AppConfig.cs
@@ -0,0 +1,21 @@
+﻿namespace Portfolio.Infratructure
+{
+    public class AppConfig
+    {
+        public TinyMCE TinyMCE { get; set; } = new TinyMCE();
+        public Company Company { get; set; } = new Company();
+    }
+    public class TinyMCE
+    {
+        public string? APIKey { get; set; }
+    }
+
+    public class Company
+    {
+        public string? CompanyName { get; set; }
+        public string? CompanyNumber { get; set; }
+        public string? CompanyShortNumber { get; set; }
+
+        public string? CompanyEmail { get; set; }
+    }
+}
--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -1,15 +1,30 @@
+using Portfolio.Infratructure;
+
 namespace Portfolio
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-            app.MapGet("/", () => "Hello World!");
+            IConfigurationBuilder configBuild = new ConfigurationBuilder()
+                .SetBasePath(builder.Environment.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
 
-            app.Run();
+            IConfiguration config = configBuild.Build();
+            AppConfig appConfig = config.GetSection("Project").Get<AppConfig>()!;
+
+            builder.Services.AddControllersWithViews();
+
+            WebApplication app = builder .Build();
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapControllerRoute("defaults", "{controller=Home}/{action=Index}/{id?}");
+
+            await app.RunAsync();
         }
     }
 }
